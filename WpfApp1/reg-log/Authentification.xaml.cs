5044ece1b49bc066623b28e1babc1d576c492909
“MegaDJV” <vladimirsheglov2000@mail.ru>
2020-05-03 00:35:35 +0300

    123

---
+++ WpfApp1/reg-log/Authentification.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interface
+{
+    public partial class Authentification : Window
+    {
+        public Authentification()
+        {
+            InitializeComponent();
+        }
+
+        public List<string> UsersInfo = new List<string>(); //Лист хранения данных пользователей (логин + пароль)
+        Registration reg = new Registration();  //Создаем поле для перехода на окно Регистрации
+
+        public void But_Conf_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < UsersInfo.Count;)   //Сравниваем данные пользователей с введенными
+            {
+                if (UsersInfo[i] == Text_Log.Text && UsersInfo[i + 1] == Text_Pass.Password)
+                    Label_Err.Content = "Вы вошли!";
+                else
+                    Label_Err.Content = "Введите данные заново";
+                i += 2;
+            }
+            if (Label_Err.Content.ToString() == "")
+                Label_Err.Content = "Вас не существует!";
+        }
+
+        public void But_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();    //Открываем окно Регистрации
+            reg.Show();
+        }
+    }
+}
---
+++ WpfApp1/reg-log/Registration.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Interface
+{
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+
+        }
+        
+        public void But_Conf_Click(object sender, RoutedEventArgs e)
+        {
+            if (Text_Log.Text == "")    //Проверяем на ошибки введенные данные
+                Label_Err.Content = "Введите логин!";
+            else if (Text_Pass.Password != Text_PassConf.Password)
+                Label_Err.Content = "Пароли не совпадают!";
+            else if (Text_Pass.Password == Text_PassConf.Password)  //Если все хорошо, передаем данные о пользователе (логин + пароль)
+            {                                                       //на хранение в лист и переходим в окно Авторизации для входа
+                Authentification Auth = new Authentification();
+                Auth.UsersInfo.Add(Text_Log.Text);
+                Auth.UsersInfo.Add(Text_Pass.Password);
+                Label_Err.Content = null;
+                this.Hide();
+                Auth.ShowDialog();
+            }
+        }
+
+        private void But_Auth_Click(object sender, RoutedEventArgs e)   //В случае мисклика просто переходим в окно Авторизации
+        {
+            this.Hide();
+            Authentification Auth = new Authentification();
+            Auth.ShowDialog();
+        }
+    }
+}
